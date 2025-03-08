cf3af342444b070a844d7d3f12010f41ad4dc499
DESKTOP-VCI486A\oscar <oscar_gs21@hotmail.com>
2025-03-08 15:06:54 +0100

    Creacion del componente y modificaciones en el servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticulosComponent } from './articulos/articulos.component';
 
 const routes: Routes = [
-
+  { path: 'articulos', component: ArticulosComponent },
+  { path: '**', redirectTo: 'articulos' }
 ];
 
 @NgModule({
